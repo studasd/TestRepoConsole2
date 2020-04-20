2880bcdb720d8b9c3c342b1a086501482b981a38
fuser85 <fuser85@myoffice365.site>
2020-04-20 19:06:08 +0300

    Добавьте файлы проекта.

---
+++ TestRepoConsole2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRepoConsole2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var MyApp = "Моя прила 2";
+
+			//var p = new NewClass();
+			//var ttr = p.newClass4;
+		}
+	}
+}
